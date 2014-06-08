5dd1603609020c693b866f9fb40c6fa1e41549a0
Yashprit Singh <yashprit@Yashprits-MacBook-Pro.local>
2014-06-08 20:32:30 +0530

    added css-minify to grunt task, inluding sequance task to it

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,11 @@ var jshint = require('gulp-jshint'),
     rename = require('gulp-rename'),
     jeditor = require('gulp-json-editor'),
     concatCss = require('gulp-concat-css'),
-    clean = require('gulp-clean');
+    clean = require('gulp-clean'),
+	sequence = require('run-sequence'),
+	minify = require('gulp-minify-css'),
+	csslint = require('gulp-csslint'),
+	pkg = require('./package.json');
 
 
 var source = {
@@ -35,18 +39,36 @@ var source = {
     },
     chrome = {
         path: 'dist/build/chrome/'
-    }
+    },
+	manifest = {
+		content_scripts: [{
+        	'matches': ['https://github.com/*'],
+            'css': [filename.css],
+            'js': [filename.js]
+         }],
+		 name: pkg.name,
+		 version: pkg.version,
+		 description: pkg.description,
+		 author: pkg.author,
+		 homepage_url: pkg.homepage
+	}
 
     // Lint Task
 gulp.task('lint', function() {
+	
+    gulp.src(source.css)
+       .pipe(csslint())
+       .pipe(csslint.reporter('default'));
+	
     return gulp.src(source.main)
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
+
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src([source.jquery, source.pjax, source.sidr, source.easytree, source.main])
+    return gulp.src([source.github, source.jquery, source.pjax, source.sidr, source.easytree, source.main])
         .pipe(concat(filename.js))
         .pipe(stripDebug())
         .pipe(uglify())
@@ -56,29 +78,37 @@ gulp.task('scripts', function() {
 gulp.task('css', function() {
     return gulp.src(source.css)
         .pipe(concatCss(filename.css))
+		.pipe(minify({keepBreaks:true}))
         .pipe(gulp.dest(build.css));
 
 });
 
-gulp.task('build:chrome', ['lint', 'css', 'scripts'], function() {
+gulp.task('copy', function(){
     gulp.src(build.css + filename.css)
         .pipe(gulp.dest(chrome.path));
 
-    gulp.src(build.js + filename.js)
+    return gulp.src(build.js + filename.js)
         .pipe(gulp.dest(chrome.path));
+});
 
-    return gulp.src(source.manifest)
-        .pipe(jeditor({
-            'content_scripts': [{
-                'matches': ['https://github.com/*'],
-                'css': [filename.css],
-                'js': [filename.js]
-            }]
-        }))
-        .pipe(gulp.dest(chrome.path));
+gulp.task('build:chrome', function() {
+	return gulp.src(source.manifest)
+			   .pipe(jeditor(function(json) {
+				   Object.keys(manifest).forEach(function (item) {
+				       json[item] = manifest[item];
+				   });
+			return json;
+        })).pipe(gulp.dest(chrome.path));
 });
 
 // Default Task
 gulp.task('default', ['lint', 'scripts', 'css']);
 
-gulp.task('chrome', ['build:chrome']);
+gulp.task('chrome', function(callback) {
+  sequence(
+        'default',
+        'copy',
+        'build:chrome',
+        callback
+    );
+});
