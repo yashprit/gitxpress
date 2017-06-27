660a6cc3fc2db340ea3368a6f08742b3e549081b
Yashprit Singh <yashprits@gmail.com>
2017-06-27 13:09:46 +0530

    test commit for author

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -5,7 +5,6 @@ ext.runtime.onMessage.addListener(
     if(request.action === "perform-save") {
       console.log("Extension Type: ", "/* @echo extension */");
       console.log("PERFORM AJAX", request.data);
-
       sendResponse({ action: "saved" });
     }
   }
